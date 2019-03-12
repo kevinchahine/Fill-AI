b234524dfe5106cc08153069371d5835f35772ca
Kevin Chahine <kchahine17@gmail.com>
2019-03-11 23:08:05 -0400

    Found a runtime error with FillGame::moveUp_Fast and the other fast functions. When the function changes the value of the lines on the board, and the path is only 1 line long, we get an out of bounds exception on 'path.getCoordinate(path.size() - 2)'. size is only 1. 1 - 2 is (-1), out of bounds exception. Need to fix this next.

--- Fill/FillGame.cpp
+++ Fill/FillGame.cpp
@@ -22,16 +22,145 @@ FillGame::~FillGame() {}
 
 coord_t FillGame::moveUp()
 {
+	if (isMoveUpValid()) {
+		return moveUp_Fast();
+	}
+	else {
+		return coord_t();
+	}
+}
+
+coord_t FillGame::moveDown() {
+	if (isMoveDownValid()) {
+		return moveUp_Fast();
+	}
+	else {
+		return coord_t();
+	}
+}
+
+coord_t FillGame::moveRight() {
+	if (isMoveRightValid()) {
+		return moveUp_Fast();
+	}
+	else {
+		return coord_t();
+	}
+}
+
+coord_t FillGame::moveLeft() {
+	if (isMoveLeftValid()) {
+		return moveUp_Fast();
+	}
+	else {
+		return coord_t();
+	}
+}
+
+coord_t FillGame::moveBack() {
+	if (isMoveBackValid()) {
+		return moveUp_Fast();
+	}
+	else {
+		return coord_t();
+	}
+}
+
+bool FillGame::isMoveUpValid() const
+{
+	// 1.) Check for out of bounds
+	coord_t head = path.getCoordinate_back();
+	
+	if (head.first == 0) {
+		return false;
+	}
+
+	// 2.) Check for Collision with body or object on board
+	head.first--;
+
+	if (board.getSpace(head) != FillBoard::Empty) {
+		return false;
+	}
+
+	return true;
+}
+
+bool FillGame::isMoveDownValid() const
+{
+	// 1.) Check for out of bounds
+	coord_t head = path.getCoordinate_back();
+
+	if (head.first == board.getNRows() - 1) {
+		return false;
+	}
+
+	// 2.) Check for Collision with body or object on board
+	head.first++;
+
+	if (board.getSpace(head) != FillBoard::Empty) {
+		return false;
+	}
+
+	return true;
+}
+
+bool FillGame::isMoveRightValid() const
+{
+	// 1.) Check for out of bounds
+	coord_t head = path.getCoordinate_back();
+
+	if (head.second == board.getNCols() - 1) {
+		return false;
+	}
+
+	// 2.) Check for Collision with body or object on board
+	head.second++;
+
+	if (board.getSpace(head) != FillBoard::Empty) {
+		return false;
+	}
+
+	return true;
+
+}
+
+bool FillGame::isMoveLeftValid() const
+{
+	// 1.) Check for out of bounds
+	coord_t head = path.getCoordinate_back();
+
+	if (head.second == 0) {
+		return false;
+	}
+
+	// 2.) Check for Collision with body or object on board
+	head.second--;
+
+	if (board.getSpace(head) != FillBoard::Empty) {
+		return false;
+	}
+
+	return true;
+
+}
+
+bool FillGame::isMoveBackValid() const
+{
+	return path.size() > 1;
+}
+
+coord_t FillGame::moveUp_Fast()
+{
 	coord_t temp = path.moveUp();
-	
+
 	board.setSpaceAs(temp, (FillBoard::Space) path.getChar_back());
-		
+
 	board.setSpaceAs(path.getCoordinate(path.size() - 2), (FillBoard::Space) path.getChar(path.size() - 2));
-	
+
 	return temp;
 }
 
-coord_t FillGame::moveDown()
+coord_t FillGame::moveDown_Fast()
 {
 	coord_t temp = path.moveDown();
 
@@ -41,7 +170,7 @@ coord_t FillGame::moveDown()
 	return temp;
 }
 
-coord_t FillGame::moveRight()
+coord_t FillGame::moveRight_Fast()
 {
 	coord_t temp = path.moveRight();
 
@@ -51,7 +180,7 @@ coord_t FillGame::moveRight()
 	return temp;
 }
 
-coord_t FillGame::moveLeft()
+coord_t FillGame::moveLeft_Fast()
 {
 	coord_t temp = path.moveLeft();
 
@@ -61,63 +190,13 @@ coord_t FillGame::moveLeft()
 	return temp;
 }
 
-bool FillGame::isMoveUpValid() const
-{
-	return false;
-}
-
-bool FillGame::isMoveDownValid() const
-{
-	return false;
-}
-
-bool FillGame::isMoveRightValid() const
-{
-	return false;
-}
-
-bool FillGame::isMoveLeftValid() const
-{
-	return false;
-}
-
-bool FillGame::isMoveBackValid() const
-{
-	return false;
-}
-
-coord_t FillGame::moveBack()
+coord_t FillGame::moveBack_Fast()
 {
 	coord_t temp = path.moveBack();
 
 	board.setSpaceAs(temp, (FillBoard::Space) path.getChar_back());
-	
+
 	return temp;
-}
-
-coord_t FillGame::moveUp_Fast()
-{
-	return coord_t();
-}
-
-coord_t FillGame::moveDown_Fast()
-{
-	return coord_t();
-}
-
-coord_t FillGame::moveRight_Fast()
-{
-	return coord_t();
-}
-
-coord_t FillGame::moveLeft_Fast()
-{
-	return coord_t();
-}
-
-coord_t FillGame::moveBack_Fast()
-{
-	return coord_t();
 }
 
 void FillGame::print() const
