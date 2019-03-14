5088d5ee3ce876830f411936d8ad1a07ecb9c93b
Kevin Chahine <kchahine17@gmail.com>
2019-03-13 20:28:51 -0400

    Created an empty UserInterface class and started drawing out a UserInterface in MS paint

---
+++ Fill/UserInterface.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+
+#include "FillBoard.h"
+#include "FillGame.h"
+#include "FillPath.h"
+
+using namespace std;
+
+class UserInterface
+{
+public:
+	UserInterface();
+	~UserInterface();
+};
+
